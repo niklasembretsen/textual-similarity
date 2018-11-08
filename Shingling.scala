26e3792b1ee688a7902922ec3f90d63c69d7ebe9
Dusan <mac@n143-p24.eduroam.kth.se>
2018-11-08 12:02:41 +0100

    Shingling

--- Shingling.scala
+++ Shingling.scala
@@ -5,31 +5,39 @@ import java.nio.ByteBuffer
 
 object Shingling {
 
-	def main(args: Array[String]) {
-		shingling("test.txt", 5)
+	def main(args:Array[String]){
+		val docSet1 = shingling("test.txt", 2)
+		val docSet2 = shingling("test2.txt", 2)
+		val comparison = jaccardSimilarity(docSet1, docSet2)
+		println(comparison)
 	}
 
-	def shingling(filename: String, k: Int) {
-
-		//A set for holding the shingles of a document
-		var documentShingles: Set[String] = Set()
-
-		//fetch each line from the specified file
-		for (line <- Source.fromFile(filename).getLines) {
-			//get the length of the line to know how far to go when
-			//creating the k-shingles of the line. Finally add
-			//each shingle to the set.
-			var lineLength = line.length
-			for (i <- 0 to (lineLength - k)) {
-				var shingle = line.slice(i, i + k)
-				documentShingles += shingle
-			}
+	def jaccardSimilarity (document1: TreeSet[Int],document2: TreeSet[Int]): Double = {
+			val similarity = document1.intersect(document2).size / document1.union(document2).size
+			similarity
 		}
-		val hashedSingles = documentShingles
-			.map(word => word.hashCode())
 
-		val orderedHashedShingles = TreeSet[Int]() ++ hashedSingles
-		documentShingles.foreach(println)
-		orderedHashedShingles.foreach(println)
-	}
-}
+	def shingling (filename: String, k: Int): TreeSet[Int] = {
+
+			//A set for holding the shingles of a document
+			var documentShingles: Set[String] = Set()
+
+			//fetch each line from the specified file
+			for(line <- Source.fromFile(filename).getLines) {
+				//get the length of the line to know how far to go when
+				//creating the k-shingles of the line. Finally add
+				//each shingle to the set.
+				var lineLength = line.length
+				for(i <- 0 to (lineLength - k)) {
+					var shingle = line.slice(i, i + k)
+					documentShingles += shingle
+				}
+			}
+			val hashedSingles = documentShingles
+				.map(word => word.hashCode())
+
+			val orderedHashedShingles = TreeSet[Int]() ++ hashedSingles
+			orderedHashedShingles
+		}
+
+}	
